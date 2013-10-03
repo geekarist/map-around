c1bdbf0ec3e14f22a3e05c563b713f1ddc7d2e70
Christophe <christophe.pele@gmail.com>
2013-10-03 14:04:55 +0200

    Use degrees in bearing method

--- mapar/src/main/java/com/geekarist/mapar/App.java
+++ mapar/src/main/java/com/geekarist/mapar/App.java
@@ -9,21 +9,27 @@ import static java.lang.Math.toRadians;
 
 public class App {
 	public static void main(String[] args) {
-		// From 9 r. cx faubin
-		double lat1 = toRadians(48.85845);
-		double lon1 = toRadians(2.38548);
-		// To 2 pl. la défense
-		System.out.printf("To 2 defense: %f\n", bearing(lat1, lon1, toRadians(48.89388), toRadians(2.24014)));
-		// To 2 rue a. honegger
-		System.out.printf("To 2 honegger: %f\n", bearing(lat1, lon1, toRadians(48.72899), toRadians(2.57943)));
+		System.out.printf("From 9 r. cx faubin to 2 defense: %f\n",
+				bearing(48.85845, 2.38548, 48.89388, 2.24014));
+		System.out.printf("From 9 r. cx faubin to 2 honegger: %f\n",
+				bearing(48.85845, 2.38548, 48.72899, 2.57943));
+
+		// From Osaka to Bagdad
+		System.out.printf("From Osaka to Bagdad: %f\n",
+				bearing(35, 45, 35, 135));
 	}
 
 	private static double bearing(double lat1, double lon1, double lat2,
 			double lon2) {
-		double dlon = lon1 - lon2;
-		double bearing = atan2( //
-				sin(dlon) * cos(lat2), //
-				cos(lat1) * sin(lat2) - sin(lat1) * cos(lat2) * cos(dlon));
-		return toDegrees(bearing);
+		double lat2Rad = toRadians(lat2);
+		double lat1Rad = toRadians(lat1);
+		double lon2Rad = toRadians(lon2);
+		double lon1Rad = toRadians(lon1);
+		double dlonRad = lon2Rad - lon1Rad;
+		double bearingRad = atan2( //
+				sin(dlonRad) * cos(lat2Rad), //
+				cos(lat1Rad) * sin(lat2Rad) - sin(lat1Rad) * cos(lat2Rad)
+						* cos(dlonRad));
+		return toDegrees(bearingRad);
 	}
 }
